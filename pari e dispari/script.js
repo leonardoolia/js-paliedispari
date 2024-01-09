e2774610e2f0b9a2d209e5ce01fdc2d926e81012
leonardoolia <leonardooliaa@gmail.com>
2024-01-09 16:52:51 +0100

    created function for random number

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -8,5 +8,15 @@ const formNumber = document.getElementById('quantity');
 const pcResultParagraph = document.getElementById('pc-result');
 const finalResultParagraph = document.getElementById('final-result');
 
+//? 2. Funzione per generare numero casuale da 1 a 5 per il pc
+function getRandomNumber() {
+    return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+};
+const randomNumber = getRandomNumber();
+
+
+
+
+
 
 
