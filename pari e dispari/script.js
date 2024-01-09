456e3570f49749b1c877cca2ade9d01fa94cb354
leonardoolia <leonardooliaa@gmail.com>
2024-01-09 17:30:03 +0100

    ex2 - fixed event listener

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -8,19 +8,17 @@ const formNumber = document.getElementById('quantity');
 const pcResultParagraph = document.getElementById('pc-result');
 const finalResultParagraph = document.getElementById('final-result');
 
-//? 2. Funzione per generare numero casuale da 1 a 5 per il pc
-function getRandomNumber() {
-    return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-};
-const randomNumber = getRandomNumber();
 
-
-
-
-//? 3. Aggiungo event listener
+//? 2. Aggiungo event listener
 
 formField.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    // 3. Funzione per generare numero casuale da 1 a 5 per il pc
+    function getRandomNumber() {
+        return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    };
+    const randomNumber = getRandomNumber();
 
     // Richiamo il numero casuale del pc
     getRandomNumber();
@@ -34,10 +32,22 @@ formField.addEventListener('submit', function (e) {
     console.log(sum);
 
 
+    //! Funzione per determinare se l'utente ha scelto pari o dispari
+    function getChoice() {
+        const choice = document.querySelectorAll('input[name="choice"]:checked').value;
+
+        console.log(choice);
+    };
 
 
+    // Determino il risultato
+    let message = '';
 
-
+    if (sum % 2 === 0 && evenForm) {
+        message = 'Congratulazioni, hai vinto'
+    } else {
+        message = 'Spiacente, ha vinto il pc'
+    };
 
 
 
