0fab0027d396c14bc715034b7e689368d8f8cce5
leonardoolia <leonardooliaa@gmail.com>
2024-01-09 16:52:25 +0100

    ex2 - got element from html

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,1 +1,12 @@
-console.log('pari e dispari')
+//? 1. Recupero elementi dal DOM
+
+const formField = document.getElementById('form');
+const evenForm = document.getElementById('even');
+const oddForm = document.getElementById('odd');
+const formNumber = document.getElementById('quantity');
+
+const pcResultParagraph = document.getElementById('pc-result');
+const finalResultParagraph = document.getElementById('final-result');
+
+
+
